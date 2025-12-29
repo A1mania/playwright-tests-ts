e3a5c2adbd60ccfa0b4ddccb36068006dc20ad97
Anna Pachkovskaya <106625221+A1mania@users.noreply.github.com>
2025-12-29 14:25:27 +0300

    minor changes

--- tests/test-browser-actions/Task7.spec.ts
+++ tests/test-browser-actions/Task7.spec.ts
@@ -11,17 +11,17 @@ test.beforeEach(async ({ page }) => {
   await page.goto("https://the-internet.herokuapp.com/download");
 });
 
-test("check file downloaded successfuly", async ({ page }) => {
-  const downloadPromise = page.waitForEvent("download");
+// test("check file downloaded successfuly", async ({ page }) => {
+//   const downloadPromise = page.waitForEvent("download");
 
-  await page.getByText("test_file.txt").click();
-  const download = await downloadPromise;
-  downloadPath = "tests/download/" + download.suggestedFilename();
-  await download.saveAs(downloadPath);
+//   await page.getByText("test_file.txt").click();
+//   const download = await downloadPromise;
+//   downloadPath = "tests/download/" + download.suggestedFilename();
+//   await download.saveAs(downloadPath);
 
-  const fileData = readFileSync(downloadPath, "utf-8");
-  expect(fileData.trim()).toBe("Test file content");
-});
+//   const fileData = readFileSync(downloadPath, "utf-8");
+//   expect(fileData.trim()).toBe("Test file content");
+// });
 
 test.afterEach(() => {
   unlinkSync(downloadPath);
